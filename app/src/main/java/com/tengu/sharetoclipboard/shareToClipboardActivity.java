e9930592a8e07e3717532360eaeff7c3b1ff58d2
Tal Hacohen <hacohen.tal@gmail.com>
2015-01-12 16:36:05 +0200

    Add support for v-card sharing

--- app/src/main/java/com/tengu/sharetoclipboard/shareToClipboardActivity.java
+++ app/src/main/java/com/tengu/sharetoclipboard/shareToClipboardActivity.java
@@ -4,12 +4,24 @@ import android.annotation.SuppressLint;
 import android.app.Activity;
 import android.content.ClipData;
 import android.content.ClipboardManager;
+import android.content.ContentResolver;
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
+import android.provider.ContactsContract.Contacts;
 import android.widget.Toast;
 
 import org.apache.http.protocol.HTTP;
+
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
+
+import ezvcard.Ezvcard;
+import ezvcard.VCard;
+import ezvcard.property.Email;
+import ezvcard.property.Telephone;
 
 public class shareToClipboardActivity extends Activity {
     @Override
@@ -21,13 +33,65 @@ public class shareToClipboardActivity extends Activity {
         String action = intent.getAction();
         String type = intent.getType();
 
-        if (Intent.ACTION_SEND.equals(action) && type != null &&  HTTP.PLAIN_TEXT_TYPE.equals(type))
-            handleSendText(intent);
-
+        if (Intent.ACTION_SEND.equals(action) && type != null) {
+            if (HTTP.PLAIN_TEXT_TYPE.equals(type))
+                handleSendText(intent);
+            if (Contacts.CONTENT_VCARD_TYPE.equals(type))
+                handleSendVCard(intent);
+        }
         finish();
     }
 
-    @SuppressLint("NewApi")
+    private void handleSendVCard(Intent intent)
+    {
+        Uri uri = (Uri) intent.getExtras().get(Intent.EXTRA_STREAM);
+        InputStream stream = null;
+        ContentResolver cr = getContentResolver();
+        try {
+            stream = cr.openInputStream(uri);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        StringBuffer fileContent = new StringBuffer("");
+        int ch;
+        try {
+            while((ch = stream.read()) != -1) {
+                fileContent.append((char) ch);
+            }
+            stream.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        VCard vcard = Ezvcard.parse(new String(fileContent)).first();
+        String fullName = vcard.getFormattedName().getValue();
+        String phone ="";
+        for (Telephone telephone : vcard.getTelephoneNumbers()){
+            phone+= arrayToString(telephone.getTypes().toArray()) + ": " + telephone.getText() + "\n";
+        }
+        String emailString ="";
+        for (Email email: vcard.getEmails()){
+            emailString+= arrayToString(email.getTypes().toArray()) + ": " + email.getValue() + "\n";
+        }
+        copyToClipboard(fullName +"\n"+phone+emailString);
+
+    }
+
+    private String arrayToString(Object[] objectArray){
+        String return_value = "";
+        for(Object value: objectArray){
+            if (!value.toString().equals("pref"))
+                return_value += value.toString().substring(0, 1).toUpperCase() + value.toString().substring(1).toLowerCase();
+        }
+        if (return_value.equals(""))
+            return_value = "Other";
+        return return_value;
+    }
+
+    private void showToast(String text) {
+        Toast.makeText(this, text, Toast.LENGTH_LONG).show();
+    }
+
     private void handleSendText(Intent intent) {
         String sharedText = intent.getStringExtra(Intent.EXTRA_TEXT);
         String sharedTitle = intent.getStringExtra(Intent.EXTRA_SUBJECT);
@@ -43,13 +107,16 @@ public class shareToClipboardActivity extends Activity {
             clipboardText += "\n";
         if (sharedText != null)
             clipboardText+= sharedText;
+        copyToClipboard(clipboardText);
+    }
 
-
+    @SuppressLint("NewApi")
+    private void copyToClipboard(String clipboardText){
         int sdk = Build.VERSION.SDK_INT;
         if (sdk < Build.VERSION_CODES.HONEYCOMB) {
             ClipboardManager clipboard = (ClipboardManager) this
                     .getSystemService(this.CLIPBOARD_SERVICE);
-            clipboard.setText(sharedText);
+            clipboard.setText(clipboardText);
         } else {
             ClipboardManager clipboard = (android.content.ClipboardManager) this
                     .getSystemService(this.CLIPBOARD_SERVICE);
@@ -58,6 +125,7 @@ public class shareToClipboardActivity extends Activity {
                             "text", clipboardText);
             clipboard.setPrimaryClip(clip);
         }
+
     }
 
 }
