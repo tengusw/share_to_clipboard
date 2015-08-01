40446191c03b3dafde2fd3d568640a2508e9c5ce
Tal Hacohen <hacohen.tal@gmail.com>
2015-08-02 00:17:04 +0300

    Add support for mailto

--- app/src/main/java/com/tengu/sharetoclipboard/shareToClipboardActivity.java
+++ app/src/main/java/com/tengu/sharetoclipboard/shareToClipboardActivity.java
@@ -33,6 +33,8 @@ public class shareToClipboardActivity extends Activity {
         Intent intent = getIntent();
         String action = intent.getAction();
         String type = intent.getType();
+        String scheme = intent.getScheme();
+
         if (Intent.ACTION_SEND.equals(action) && type != null) {
             if (HTTP.PLAIN_TEXT_TYPE.equals(type))
                 handleSendText(intent);
@@ -40,20 +42,20 @@ public class shareToClipboardActivity extends Activity {
                 handleSendVCard(intent);
             else
                 showToast(getString(R.string.error_type_not_supported));
-        } else if ((Intent.ACTION_VIEW.equals(action) || Intent.ACTION_DIAL.equals(action)
-                && intent.getScheme().equals("tel")) ) {
-            handleDial(intent);
+        } else if ((Intent.ACTION_VIEW.equals(action) || Intent.ACTION_DIAL.equals(action))
+                && (scheme.equals("tel") || scheme.equals("mailto"))) {
+            handleSchemeSpecificPart(intent);
         }
         finish();
     }
 
-    private void handleDial(Intent intent) {
-        String calledNumber;
+    private void handleSchemeSpecificPart(Intent intent) {
+        String dataString;
         String inputURI = intent.getDataString();
         if (inputURI != null) {
             Uri uri = Uri.parse(Uri.decode(inputURI));
-            calledNumber = uri.getSchemeSpecificPart();
-            copyToClipboard(calledNumber);
+            dataString = uri.getSchemeSpecificPart();
+            copyToClipboard(dataString);
         } else {
             showToast(getString(R.string.error_no_data));
         }
