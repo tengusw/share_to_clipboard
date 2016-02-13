83366b2e419de9ed5e78a9a293815e55355fbafb
Tal Hacohen <hacohen.tal@gmail.com>
2016-02-13 14:01:06 +0200

    Reduce apk size. Remove app compat.

--- app/src/main/java/com/tengu/sharetoclipboard/MainActivity.java
+++ app/src/main/java/com/tengu/sharetoclipboard/MainActivity.java
@@ -1,16 +1,15 @@
 package com.tengu.sharetoclipboard;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     private static final String TENGU_URL = "http://tengu.it/android-apps";
 
@@ -32,18 +31,12 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void setUpToolbar() {
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
-        if (getSupportActionBar() != null) {
-            getSupportActionBar().setDisplayShowTitleEnabled(false);
-        }
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             Window window = getWindow();
             window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
             window.clearFlags(WindowManager.LayoutParams.FLAG_TRANSLUCENT_STATUS);
             window.setStatusBarColor(getResources().getColor(R.color.primary600));
         }
-
     }
 
 }
