fba1073592cfc653ebbd2caf433ef9ddc6281bf9
Hugo Musso Gualandi <hugo_musso_gualandi@hotmail.com>
2020-05-17 22:38:52 -0300

    Show a toast if there is no notification
    This ensures that there is always visual feedback when using share to
    clipboard. Fixes #33.

--- app/src/main/java/com/tengu/sharetoclipboard/ShareToClipboardActivity.java
+++ app/src/main/java/com/tengu/sharetoclipboard/ShareToClipboardActivity.java
@@ -184,6 +184,8 @@ public class ShareToClipboardActivity extends Activity {
 
         if (PreferenceUtil.shouldDisplayNotification(this)) {
             NotificationUtil.createNotification(this);
+        } else {
+            showToast(getString(R.string.notification_title));
         }
     }
 }
